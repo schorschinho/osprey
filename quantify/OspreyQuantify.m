4b7762cd1c48faf99fbd5c1aba95e47abd8cd73b
Helge <47348963+hezoe100@users.noreply.github.com>
2019-11-04 14:07:23 -0500

    Added metabolite table
    Intigrated metabolite output as table format and metabolite combinations.

--- quantify/OspreyQuantify.m
+++ quantify/OspreyQuantify.m
@@ -524,3 +524,18 @@ end
 end
 
 %%% / Lookup function for metabolite relaxation times %%%
+
+%%% Function to create metaboite overview in MATLAB table format %%%
+function [MRSCont] = ops_createTable(MRSCont,qtfyType)
+
+    % Extract metabolite names from basisset
+    names = MRSCont.quantify.metabs;
+
+    conc = zeros(MRSCont.nDatasets,length(names));
+    for kk = 1:MRSCont.nDatasets
+        conc(kk,:) = MRSCont.quantify.(qtfyType){kk}';
+    end
+    % Save back to Osprey data container
+    MRSCont.quantify.tables.(qtfyType)  = array2table(conc,'VariableNames',names);
+end
+%%% Function to create metaboite overview in MATLAB table format %%%
