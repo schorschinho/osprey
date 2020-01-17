1c28de8806b854fad9581ad6dc2ccf99e324bdac
schorschinho <goeltzs1@jhmi.edu>
2020-01-17 12:25:03 -0500

    Add LCModel-like model.
    Add a new quantification model (frequency-domain real part fitting with arbitrary lineshape; similar to LCModel, but as of today unregularized baseline). Modify plot functions. Add L-BFGS-B algorithm library as the solver of the linear problem during the fitting.

---
+++ libraries/FID-A/fitTools/fitModels/LCModel/fit_LCModel.m
@@ -0,0 +1,99 @@
+% fit_LCModel.m
+% Georg Oeltzschner, Johns Hopkins University 2019.
+%
+% USAGE:
+% [fitParams] = fit_LCModel(dataToFit, resBasisSet, fitOpts);
+% 
+% DESCRIPTION:
+% This is the function describing the LCModel fitting model, which is
+% performed with the resampled basis set resBasisSet on the data contained
+% in the FID-A data structure dataToFit, using the fit options in the
+% structure fitOpts.
+%
+% This model is attempting to emulate the rather elusive original LCModel
+% algorithm (Provencher, Magn Reson Med 30:672-679 (1993)).
+% 
+% OUTPUTS:
+% fitParams   = Structure containing all necessary fit parameters
+%                   - ampl: amplitudes for metabolite/MM/lipids
+%                   - beta_j: amplitudes for baseline spline functions
+%                   - lineshape: coefficients for lineshape convolution
+%                   - ph0: zero-order phase correction
+%                   - ph1: first-order phase correction
+%                   - gaussLB: Gaussian dampening (common to all basis
+%                       functions)
+%                   - lorentzLB: Lorentzian dampening (individual to each
+%                       basis function)
+%                   - freqShift: frequency shift (individual to each basis
+%                       function)
+%
+% INPUTS:
+% dataToFit   = FID-A data structure
+% basisSet    = FID-A basis set container
+% fitOpts     = Structure containing fit options
+
+function [fitParams, resBasisSet] = fit_LCModel(dataToFit, basisSet, fitOpts)
+
+%%% 0. PREPARE DATA AND BASIS SET %%%
+dataToFit               = op_zeropad(dataToFit, 2);
+% Resample basis set to match data resolution and frequency range
+resBasisSet             = fit_resampleBasis(dataToFit, basisSet);
+
+
+%%% 1. EXTRACT OPTIONS AND PREPARE FIT %%%
+% Extract ppm fit range
+fitRangePPM             = fitOpts.range;
+% Initialize the baseline spline parameters
+minKnotSpacingPPM       = fitOpts.bLineKnotSpace; % this is the DKNTMN parameter in LCModel
+
+
+%%% 2. INITIAL REFERENCING %%%
+% Determine initial coarse frequency shift from cross-correlation with
+% landmark delta functions for NAA, Cr, Cho.
+disp('Running initial referencing...');
+[refShift, refFWHM] = fit_LCModelReferencing(dataToFit);
+% Apply initial referencing shift
+dataToFitRef = op_freqshift(dataToFit, -refShift);
+
+
+%%% 3. PRELIMINARY ANALYSIS STEP 1 %%%
+% In step 1 of the preliminary analysis, a reduced basis set (Cr, Glu, Ins,
+% GPC, NAA) is used to obtain a first estimate for the frequency shift
+% and the zero- and first-order phase shift parameters.
+% Well-phased spectra should be fine with the starting values for the phase
+% corrections, which are zero degrees (zero order) and zero degrees/ppm
+% (first order).
+%
+% The preliminary analysis performed by LCModel allows to cycle those
+% starting values, i.e. do the preliminary analysis with pairs of e.g. [30,
+% 30] etc. - this is governed by the choice of the DEGZER/SDDEGZ and
+% DEGPPM/SDDEGP parameters.
+%
+% In that case, the preliminary analysis is run multiple times, and the
+% best phasing / referencing shift parameters are chosen.
+% (Worth exploring in future versions by passing the starting values for
+% the phase corrections as arguments.)
+disp('Running preliminary analysis with reduced basis set...');
+[fitParamsStep1] = fit_LCModel_PrelimReduced(dataToFitRef, resBasisSet, minKnotSpacingPPM, fitRangePPM);
+
+
+%%% 4. FINAL PRELIMINARY ANALYSIS STEP 2 %%%
+% In the final step of the preliminary analysis, the full basis set is used
+% with the full LCModel (except for baseline regularization) to obtain
+% the final optimal starting values.
+disp('Running final preliminary analysis step with full basis set...');
+[fitParamsStep2] = fit_LCModelPrelimStep2(dataToFitRef, resBasisSet, minKnotSpacingPPM, fitRangePPM, fitParamsStep1, refFWHM);
+
+
+%%% 5. CREATE OUTPUT %%%
+% Return fit parameters
+fitParams = fitParamsStep2;
+fitParams.refShift = refShift;
+
+end
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
