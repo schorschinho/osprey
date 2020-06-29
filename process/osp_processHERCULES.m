aabf76d079df33053fb3b5e1e997aa34ad7e5ed1
Helge <47348963+hezoe100@users.noreply.github.com>
2020-06-29 17:17:51 -0400

    Revert "Analytic Jacobian"
    This reverts commit 4bc697472e70ce8babf55b62748f9c596e41fd00.

--- process/osp_processHERCULES.m
+++ process/osp_processHERCULES.m
@@ -282,13 +282,13 @@ for kk = 1:MRSCont.nDatasets
         % between the common 'reporter' signals.
         [raw_A, raw_B, raw_C, raw_D] = osp_editSubSpecAlign(raw_A, raw_B, raw_C, raw_D);
         % Create the sum spectrum
-        Sum     = op_addScans(raw_A,raw_B);
-        Sum     = op_addScans(Sum,raw_C);
-        Sum     = op_addScans(Sum,raw_D);
-        Sum.commuteOrder = commuteOrder;
-        Sum.specReg.fs = fs;
-        Sum.specReg.phs = phs;
-        Sum.specReg.weights = weights;
+        sum     = op_addScans(raw_A,raw_B);
+        sum     = op_addScans(sum,raw_C);
+        sum     = op_addScans(sum,raw_D);
+        sum.commuteOrder = commuteOrder;
+        sum.specReg.fs = fs;
+        sum.specReg.phs = phs;
+        sum.specReg.weights = weights;
         % Create the GABA-edited difference spectrum
         diff1   = op_addScans(raw_B,raw_D);
         diff1   = op_addScans(diff1,raw_A,1);
@@ -378,22 +378,22 @@ for kk = 1:MRSCont.nDatasets
         diff2   = diff2_temp;
         diff2   = op_fddccorr(diff2,100);                                 % Correct back to baseline
 
-        [Sum_temp,~,~]          = op_removeWater(Sum,[4.5 4.9],20,0.75*length(Sum.fids),0); % Remove the residual water
-        if isnan(real(Sum_temp.fids))
+        [sum_temp,~,~]          = op_removeWater(sum,[4.5 4.9],20,0.75*length(sum.fids),0); % Remove the residual water
+        if isnan(real(sum_temp.fids))
             rr = 30;
-            while isnan(real(Sum_temp.fids))
-                [Sum_temp,~,~]  = op_removeWater(Sum,[4.5 4.9],rr,0.75*length(Sum.fids),0); % Remove the residual water
+            while isnan(real(sum_temp.fids))
+                [sum_temp,~,~]  = op_removeWater(sum,[4.5 4.9],rr,0.75*length(sum.fids),0); % Remove the residual water
                 rr = rr-1;
             end
         end
-        Sum     = Sum_temp;
-        Sum     = op_fddccorr(Sum,100);
+        sum     = sum_temp;
+        sum     = op_fddccorr(sum,100);
 
 
         %%% 7. REFERENCE SPECTRUM CORRECTLY TO FREQUENCY AXIS 
         % Reference resulting data correctly and consistently
         %[raw_A, refShift]   = op_ppmref(raw_A,1.8,2.2,2.008);          % Reference OFF-OFF spectrum to NAA @ 2.008 ppm
-        temp_spec = Sum;
+        temp_spec = sum;
         [refShift_final, ~] = osp_CrChoReferencing(temp_spec);% Reference OFF-OFF spectrum to NAA @ 2.008 ppm
         [raw_A]             = op_freqshift(raw_A,-refShift_final);            % Apply same shift to ON-OFF
         [raw_B]             = op_freqshift(raw_B,-refShift_final);            % Apply same shift to ON-OFF
@@ -401,7 +401,7 @@ for kk = 1:MRSCont.nDatasets
         [raw_D]             = op_freqshift(raw_D,-refShift_final);            % Apply same shift to ON-ON
         [diff1]             = op_freqshift(diff1,-refShift_final);            % Apply same shift to diff1
         [diff2]             = op_freqshift(diff2,-refShift_final);            % Apply same shift to diff2
-        [Sum]               = op_freqshift(Sum,-refShift_final);              % Apply same shift to Sum
+        [sum]               = op_freqshift(sum,-refShift_final);              % Apply same shift to sum
 
         % Add commuteOrder
         raw_A.commuteOrder = commuteOrder(1);
@@ -416,7 +416,7 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.processed.D{kk}     = raw_D;                                    % Save ON-ON back to MRSCont container
         MRSCont.processed.diff1{kk} = diff1;                                    % Save diff1 back to MRSCont container
         MRSCont.processed.diff2{kk} = diff2;                                    % Save diff2 back to MRSCont container
-        MRSCont.processed.sum{kk}   = Sum;                                      % Save sum back to MRSCont container
+        MRSCont.processed.sum{kk}   = sum;                                      % Save sum back to MRSCont container
 
 
         %%% 9. GET SHORT-TE WATER DATA %%%
@@ -450,7 +450,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.A(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.A(kk) = mean(MRSCont.QM.drift.pre.A{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.A(kk) = mean(MRSCont.QM.drift.post.A{kk} - 3.02);
-        MRSCont.QM.res_water_amp.A(kk) = sum(MRSCont.processed.A{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.B(kk)    = op_getSNR(MRSCont.processed.B{kk},2.8,3.2); % Cr amplitude over noise floor
         FWHM_Hz                 = op_getLW(MRSCont.processed.B{kk},2.8,3.2); % in Hz
@@ -458,7 +457,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.B(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.B(kk) = mean(MRSCont.QM.drift.pre.B{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.B(kk) = mean(MRSCont.QM.drift.post.B{kk} - 3.02);
-        MRSCont.QM.res_water_amp.B(kk) = sum(MRSCont.processed.B{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.C(kk)    = op_getSNR(MRSCont.processed.C{kk}); % NAA amplitude over noise floor
         FWHM_Hz                 = op_getLW(MRSCont.processed.C{kk},1.8,2.2); % in Hz
@@ -466,7 +464,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.C(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.C(kk) = mean(MRSCont.QM.drift.pre.C{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.C(kk) = mean(MRSCont.QM.drift.post.C{kk} - 3.02);
-        MRSCont.QM.res_water_amp.C(kk) = sum(MRSCont.processed.C{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.D(kk)    = op_getSNR(MRSCont.processed.D{kk},2.8,3.2); % Cr amplitude over noise floor
         FWHM_Hz                 = op_getLW(MRSCont.processed.D{kk},2.8,3.2); % in Hz
@@ -474,7 +471,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.D(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.D(kk) = mean(MRSCont.QM.drift.pre.D{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.D(kk) = mean(MRSCont.QM.drift.post.D{kk} - 3.02);
-        MRSCont.QM.res_water_amp.D(kk) = sum(MRSCont.processed.D{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.diff1(kk)    = op_getSNR(MRSCont.processed.diff1{kk},2.8,3.2); % GABA amplitude over noise floor
         FWHM_Hz                 = op_getLW(MRSCont.processed.diff1{kk},2.8,3.2); % in Hz
@@ -482,7 +478,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.diff1(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.diff1(kk) = mean(MRSCont.QM.drift.pre.diff1{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.diff1(kk) = mean(MRSCont.QM.drift.post.diff1{kk} - 3.02);
-        MRSCont.QM.res_water_amp.diff1(kk) = sum(MRSCont.processed.diff1{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.diff2(kk)    = op_getSNR(MRSCont.processed.diff2{kk},2.8,3.2); % GSH amplitude over noise floor
         FWHM_Hz                 = op_getLW(MRSCont.processed.diff2{kk},2.8,3.2); % in Hz
@@ -490,7 +485,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.diff2(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.diff2(kk) = mean(MRSCont.QM.drift.pre.diff2{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.diff2(kk) = mean(MRSCont.QM.drift.post.diff2{kk} - 3.02);
-        MRSCont.QM.res_water_amp.diff2(kk) = sum(MRSCont.processed.diff2{kk}.watersupp.amp);
 
         MRSCont.QM.SNR.sum(kk)    = op_getSNR(MRSCont.processed.sum{kk}); % Cr amplitude over noise floor
         FWHM_Hz                     = op_getLW(MRSCont.processed.sum{kk},2.8,3.2); % in Hz
@@ -498,7 +492,6 @@ for kk = 1:MRSCont.nDatasets
         MRSCont.QM.freqShift.sum(kk)  = refShift_SubSpecAlign + refShift_final;
         MRSCont.QM.drift.pre.AvgDeltaCr.sum(kk) = mean(MRSCont.QM.drift.pre.sum{kk} - 3.02);
         MRSCont.QM.drift.post.AvgDeltaCr.sum(kk) = mean(MRSCont.QM.drift.post.sum{kk} - 3.02);
-        MRSCont.QM.res_water_amp.sum(kk) = sum(MRSCont.processed.sum{kk}.watersupp.amp);
 
         if MRSCont.flags.hasRef
             MRSCont.QM.SNR.ref(kk)  = op_getSNR(MRSCont.processed.ref{kk},4.2,5.2); % water amplitude over noise floor
