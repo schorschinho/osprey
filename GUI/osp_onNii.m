de34e32de3f63ad8dd5b4e2d2c403c06e86e2cba
Helge <47348963+hezoe100@users.noreply.github.com>
2021-03-05 11:34:53 -0500

    Adding .nii.gz readability
    -Added .nii.gz reader for all vendors and overlays
    - changed standard output format for the voxel masks and segmentation maps to .nii.gz
    - replaced .nii files in the examples to reduce file size

--- GUI/osp_onNii.m
+++ GUI/osp_onNii.m
@@ -29,9 +29,20 @@ function osp_onNii( ~, ~,gui)
     fprintf('Opening external nii viever...\n');
     MRSCont = getappdata(gui.figure,'MRSCont'); % Get MRSCont from hidden container in gui class    
     if  ~((isfield(MRSCont.flags, 'isPRIAM') || isfield(MRSCont.flags, 'isMRSI')) &&  (MRSCont.flags.isPRIAM || MRSCont.flags.isMRSI))
-        nii_viewer(MRSCont.files_nii{gui.controls.Selected}, MRSCont.coreg.vol_mask{gui.controls.Selected}.fname);
+        if exist(MRSCont.coreg.vol_mask{gui.controls.Selected}.fname, 'file')
+            nii_viewer(MRSCont.files_nii{gui.controls.Selected}, MRSCont.coreg.vol_mask{gui.controls.Selected}.fname);
+        else if exist([MRSCont.coreg.vol_mask{gui.controls.Selected}.fname, '.gz'], 'file')
+            nii_viewer(MRSCont.files_nii{gui.controls.Selected}, [MRSCont.coreg.vol_mask{gui.controls.Selected}.fname, '.gz']);
+            end
+        end
     else
-         nii_viewer(MRSCont.files_nii{gui.controls.Selected}, {MRSCont.coreg.vol_mask{gui.controls.Selected}{1}.fname,MRSCont.coreg.vol_mask{gui.controls.Selected}{2}.fname})
+        if exist(MRSCont.coreg.vol_mask{gui.controls.Selected}{1}.fname, 'file')
+             nii_viewer(MRSCont.files_nii{gui.controls.Selected}, {MRSCont.coreg.vol_mask{gui.controls.Selected}{1}.fname,MRSCont.coreg.vol_mask{gui.controls.Selected}{2}.fname})
+        else if exist([MRSCont.coreg.vol_mask{gui.controls.Selected}{1}.fname, '.gz'], 'file')
+             nii_viewer(MRSCont.files_nii{gui.controls.Selected}, {[MRSCont.coreg.vol_mask{gui.controls.Selected}{1}.fname, '.gz'],[MRSCont.coreg.vol_mask{gui.controls.Selected}{2}.fname, '.gz']})
+            end
+        end
+        
     end
     fprintf('... done.\n');
     setappdata(gui.figure,'MRSCont',MRSCont); % Write MRSCont into hidden container in gui class
