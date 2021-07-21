76c353b1180229c0b752c14f2581ceb97128fffe
Helge <47348963+HJZollner@users.noreply.github.com>
2021-07-21 11:58:19 -0400

    CI #19
    - Move assertion into the test

--- ci/SinglePRESS.m
+++ ci/SinglePRESS.m
@@ -1,12 +1,4 @@
-function [exit_code] = SinglePRESS
-    % assume it always fails
-    exit_code = 1;
 
-    % MATLAB runs the test
     [result,rt] = UnitTest('SinglePRESS',0,0);
 
-    % check the result
-    if sum(rt{1}.Failed) == 0 && sum(rt{1}.Incomplete) == 0
-        exit_code = 0;
-    end
-end
+    assert(sum(rt{1}.Failed) == 0 && sum(rt{1}.Incomplete) == 0)
