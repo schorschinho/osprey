5f3313e05884312370455220f7f7cc094409f039
John LaMaster <7785925+JohnLaMaster@users.noreply.github.com>
2022-09-01 14:33:40 +0200

    Updated initial draft

--- utilities/osp_exportParam.m
+++ utilities/osp_exportParam.m
@@ -4,49 +4,65 @@ function osp_exportParam(MRSCont,path)
 % and the spectrum as a whole.
   
 names = {'ph1','gauss','gaussMM','lorentzMetab','lorentzMM','ph0','ecc','fshift'};
+num_basis_fcns = shape(MRSCont.fit.basisSet{?})
+% Define storage matrix as: [nDatasets,metab_amp,Lorentzians,Gaussians,fshifts,phi0,phi1,SNR,ecc]
+% index_dict stores names/indices
+% 18 + 8 = 26 basis functions
+parameters = zeros(MRSCont.nDatasets(1), 26 * 3 + fshifts + phi0 + phi1 + SNR + ecc)
 
-for kk = 1 : 102
-    MMdef(kk,1) = FullDefSin.fit.results.metab.fitParams{1, kk}.ph1;
-    MMupd(kk,1) = FullUpdSep.fit.results.metab.fitParams{1, kk}.ph1;
-    MM1G(kk,1)  = FullDefSin.fit.results.metab.fitParams{2, kk}.ph1;
-    MM2G(kk,1)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.ph1;
-    iMM1G(kk,1) = SingleGauss.fit.results.metab.fitParams{1, kk}.ph1;
-    iMM2G(kk,1) = SeparateGauss.fit.results.metab.fitParams{1, kk}.ph1;
+for kk = 1:MRSCont.nDatasets(1)
+    parameters(kk,1:26)    = fit.results.metab.fitParams{?, kk}.amp;
+    parameters(kk,27:52)   = fit.results.metab.fitParams{?, kk}.lorentzLB;
+    parameters(kk,53:70)   = fit.results.metab.fitParams{?, kk}.gaussLB;
+    parameters(kk,70:78)   = fit.results.metab.fitParams{?, kk}.gaussLBMM;
+    parameters(kk,79:104)  = fit.results.metab.fitParams{?, kk}.fshifts; % rad2deg()
+    parameters(kk,105)     = fit.results.metab.fitParams{?, kk}.ph0; % rad2deg()
+    parameters(kk,106)     = fit.results.metab.fitParams{?, kk}.ph1; % rad2deg()
+    parameters(kk,107)     = fit.results.metab.fitParams{?, kk}.SNR;
+    parameters(kk,107)     = fit.results.metab.fitParams{?, kk}.SNR;
+    parameters(kk,108)     = fit.results.metab.fitParams{?, kk}.ecc;
+    
+%     MMdef(kk,1) = FullDefSin.fit.results.metab.fitParams{1, kk}.ph1;
+%     MMupd(kk,1) = FullUpdSep.fit.results.metab.fitParams{1, kk}.ph1;
+%     MM1G(kk,1)  = FullDefSin.fit.results.metab.fitParams{2, kk}.ph1;
+%     MM2G(kk,1)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.ph1;
+%     iMM1G(kk,1) = SingleGauss.fit.results.metab.fitParams{1, kk}.ph1;
+%     iMM2G(kk,1) = SeparateGauss.fit.results.metab.fitParams{1, kk}.ph1;
 
-    MMdef(kk,2) = FullDefSin.fit.results.metab.fitParams{1, kk}.gaussLB;
-    MMupd(kk,2) = FullUpdSep.fit.results.metab.fitParams{1, kk}.gaussLB;
-    MM1G(kk,2)  = FullDefSin.fit.results.metab.fitParams{2, kk}.gaussLB;
-    MM2G(kk,2)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.gaussLB;
-    iMM1G(kk,2) = SingleGauss.fit.results.metab.fitParams{1, kk}.gaussLB;
-    iMM2G(kk,2) = SeparateGauss.fit.results.metab.fitParams{1, kk}.gaussLB;
+%     MMdef(kk,2) = FullDefSin.fit.results.metab.fitParams{1, kk}.gaussLB;
+%     MMupd(kk,2) = FullUpdSep.fit.results.metab.fitParams{1, kk}.gaussLB;
+%     MM1G(kk,2)  = FullDefSin.fit.results.metab.fitParams{2, kk}.gaussLB;
+%     MM2G(kk,2)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.gaussLB;
+%     iMM1G(kk,2) = SingleGauss.fit.results.metab.fitParams{1, kk}.gaussLB;
+%     iMM2G(kk,2) = SeparateGauss.fit.results.metab.fitParams{1, kk}.gaussLB;
+    
+%     MMdef(kk,3) = 0;
+%     MMupd(kk,3) = 0;
+%     MM1G(kk,3)  = 0;
+%     MM2G(kk,3)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.gaussLBMM;
+%     iMM1G(kk,3) = 0;
+%     iMM2G(kk,3) = SeparateGauss.fit.results.metab.fitParams{1, kk}.gaussLBMM;
 
-    MMdef(kk,3) = 0;
-    MMupd(kk,3) = 0;
-    MM1G(kk,3)  = 0;
-    MM2G(kk,3)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.gaussLBMM;
-    iMM1G(kk,3) = 0;
-    iMM2G(kk,3) = SeparateGauss.fit.results.metab.fitParams{1, kk}.gaussLBMM;
+%     MMdef(kk,4) = mean(FullDefSin.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
+%     MMupd(kk,4) = mean(FullUpdSep.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
+%     MM1G(kk,4)  = mean(FullDefSin.fit.results.metab.fitParams{2, kk}.lorentzLB(1:18));
+%     MM2G(kk,4)  = mean(FullUpdSep.fit.results.metab.fitParams{2, kk}.lorentzLB(1:18));
+%     iMM1G(kk,4) = mean(SingleGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
+%     iMM2G(kk,4) = mean(SeparateGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
 
-    MMdef(kk,4) = mean(FullDefSin.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
-    MMupd(kk,4) = mean(FullUpdSep.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
-    MM1G(kk,4)  = mean(FullDefSin.fit.results.metab.fitParams{2, kk}.lorentzLB(1:18));
-    MM2G(kk,4)  = mean(FullUpdSep.fit.results.metab.fitParams{2, kk}.lorentzLB(1:18));
-    iMM1G(kk,4) = mean(SingleGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
-    iMM2G(kk,4) = mean(SeparateGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(1:18));
+%     MMdef(kk,5) = mean(FullDefSin.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
+%     MMupd(kk,5) = mean(FullUpdSep.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
+%     MM1G(kk,5)  = mean(FullDefSin.fit.results.metab.fitParams{2, kk}.lorentzLB(18:end));
+%     MM2G(kk,5)  = mean(FullUpdSep.fit.results.metab.fitParams{2, kk}.lorentzLB(18:end));
+%     iMM1G(kk,5) = mean(SingleGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
+%     iMM2G(kk,5) = mean(SeparateGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
 
-    MMdef(kk,5) = mean(FullDefSin.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
-    MMupd(kk,5) = mean(FullUpdSep.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
-    MM1G(kk,5)  = mean(FullDefSin.fit.results.metab.fitParams{2, kk}.lorentzLB(18:end));
-    MM2G(kk,5)  = mean(FullUpdSep.fit.results.metab.fitParams{2, kk}.lorentzLB(18:end));
-    iMM1G(kk,5) = mean(SingleGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
-    iMM2G(kk,5) = mean(SeparateGauss.fit.results.metab.fitParams{1, kk}.lorentzLB(18:end));
-
-    MMdef(kk,6) = FullDefSin.fit.results.metab.fitParams{1, kk}.ph0;
-    MMupd(kk,6) = FullUpdSep.fit.results.metab.fitParams{1, kk}.ph0;
-    MM1G(kk,6)  = FullDefSin.fit.results.metab.fitParams{2, kk}.ph0;
-    MM2G(kk,6)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.ph0;
-    iMM1G(kk,6) = SingleGauss.fit.results.metab.fitParams{1, kk}.ph0;
-    iMM2G(kk,6) = SeparateGauss.fit.results.metab.fitParams{1, kk}.ph0;
+%     MMdef(kk,6) = FullDefSin.fit.results.metab.fitParams{1, kk}.ph0;
+%     MMupd(kk,6) = FullUpdSep.fit.results.metab.fitParams{1, kk}.ph0;
+%     MM1G(kk,6)  = FullDefSin.fit.results.metab.fitParams{2, kk}.ph0;
+%     MM2G(kk,6)  = FullUpdSep.fit.results.metab.fitParams{2, kk}.ph0;
+%     iMM1G(kk,6) = SingleGauss.fit.results.metab.fitParams{1, kk}.ph0;
+%     iMM2G(kk,6) = SeparateGauss.fit.results.metab.fitParams{1, kk}.ph0;
 
 end
 
