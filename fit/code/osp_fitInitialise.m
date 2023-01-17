5d7238edb1e91448e2c419ea4ec387cae768b49f
schorschinho <schorschinho@users.noreply.github.com>
2023-01-17 11:10:09 -0500

    Update osp_fitInitialise.m
    Moving Bo and TE determination out of an if statement to fix an error that occurred when fitting via the LCModel binary was attempted.

--- fit/code/osp_fitInitialise.m
+++ fit/code/osp_fitInitialise.m
@@ -42,18 +42,19 @@ if ~strcmp(seq,'press') && ~strcmp(seq,'slaser') && ~strcmp(seq,'special') %Unab
     seq = 'press';
 end
 
+% Extract TE, B0, and sequence from first dataset
+te = num2str(MRSCont.raw{1}.te);
+Bo = MRSCont.raw{1}.Bo;
+if (Bo >= 2.8 && Bo < 3.1)
+    Bo = '3T';
+else
+    Bo = '7T';
+end
+    
 % Find the right basis set (provided as *.mat file in Osprey basis set
 % format)
 if ~(isfield(MRSCont.opts.fit,'basisSetFile') && ~isempty(MRSCont.opts.fit.basisSetFile) && ~isfolder(MRSCont.opts.fit.basisSetFile))
 
-    % Extract TE, B0, and sequence from first dataset
-    te = num2str(MRSCont.raw{1}.te);
-    Bo = MRSCont.raw{1}.Bo;
-    if (Bo >= 2.8 && Bo < 3.1)
-        Bo = '3T';
-    else
-        Bo = '7T';
-    end
     % Intercept non-integer echo times and replace the decimal point with
     % an underscore to avoid file extension problems
     if contains(te, '.')
@@ -183,25 +184,25 @@ end
 % The workflow will differ depending on whether we fit entirely within
 % Osprey, or whether we are wrapping the LCModel binaries.
 switch MRSCont.opts.fit.method
-
+    
     % ------ OPTION OSPREY -----
     case 'Osprey'
-
+        
         % Load the specified basis set or the user basis set file
         basisSet = load(MRSCont.opts.fit.basisSetFile);
         basisSet = basisSet.BASIS;
-
+        
         % Add basis spectra (if they were removed to reduce thhe file size)
         if ~isfield(basisSet,'specs')
             [basisSet]=osp_recalculate_basis_specs(basisSet);
         end
-
+        
         % Generate the list of basis functions that are supposed to be included in
         % the basis set
         if ext
             % Sort basis set file according to Osprey conventions
             basisSet = fit_sortBasisSet(basisSet);
-
+            
             % To do: Interface with interactive user input
             metabList = fit_createMetabList(MRSCont.opts.fit.includeMetabs);
             % Collect MMfit flag from the options determined in the job file
@@ -217,7 +218,7 @@ switch MRSCont.opts.fit.method
             % Create the modified basis set
             basisSet = fit_selectMetabs(basisSet, metabList, fitMM);
         end
-
+        
         % Determine the scaling factor between data and basis set for each dataset
         for kk = 1:MRSCont.nDatasets(1)
             if ~MRSCont.flags.isMRSI  && ~MRSCont.flags.isPRIAM
@@ -231,26 +232,27 @@ switch MRSCont.opts.fit.method
                 MRSCont.fit.scale{kk} = max(max(max(real(MRSCont.processed.A{kk}.specs)))) / max(max(max(real(basisSet.specs))));
             end
         end
-
-
+        
+        
         % Save the modified basis set
         MRSCont.fit.basisSet = basisSet;
-
-
-
-    % ------ OPTION LCMODEL -----
+        
+        
+        
+        % ------ OPTION LCMODEL -----
     case 'LCModel'
-        %Write a sequence name into the fit struct as it is needed for some
-        %downstream functions
+        
+        % Write a sequence name into the fit struct as it is needed for some
+        % downstream functions
         MRSCont.fit.basisSet.seq{1} = seq;
+        
         % For now, the user needs to EXPLICITLY specify a basis set. We
         % will weave in the automatic selection as we convert more and more
         % basis sets to LCModel format.
         % (GO 07/08/2021)
-
-
+        
         if ~(isfield(MRSCont.opts.fit,'basisSetFile') && ~isempty(MRSCont.opts.fit.basisSetFile))
-
+            
             % The only exception are LCModel sptype settings (like lipid-8)
             % which don't use external basis functions at all.
             % These modes, however, still require a dummy basis set to be
@@ -262,7 +264,7 @@ switch MRSCont.opts.fit.method
                 if ~isempty(MRSCont.opts.fit.controlFile)
                     % Load all control parameters
                     LCMparam = osp_readlcm_control(MRSCont.opts.fit.controlFile);
-
+                    
                     if isfield(LCMparam, 'sptype')
                         switch LCMparam.sptype
                             case {'''lipid-8''', '''liver-11''', '''breast-8''', '''only-cho-2'''}
@@ -274,12 +276,12 @@ switch MRSCont.opts.fit.method
                                 basisSetFile = LCMparam.filbas;
                         end
                     end
-
+                    
                 end
             else
                 error('For LCModel fitting, please explicitly specify a .BASIS file in the job file (opts.fit.basisSetFile = ''FILE'').');
             end
-
+            
         else
             
             % If a basis set file is supplied, use it.
@@ -292,9 +294,9 @@ switch MRSCont.opts.fit.method
                 % to LCModel format (.basis)
                 basisSet = load(MRSCont.opts.fit.basisSetFile);
                 basisSet = basisSet.BASIS;
-                % Add basis spectra (if they were removed to reduce thhe file size)
+                % Add basis spectra (if they were removed to reduce the file size)
                 if ~isfield(basisSet,'specs')
-                    [basisSet]=osp_recalculate_basis_specs(basisSet);
+                    [basisSet] = osp_recalculate_basis_specs(basisSet);
                 end
                 [~]      = io_writelcmBASIS(basisSet,[path filesep Bo '_' seq '_' MRSCont.vendor '_' te 'ms_noMM.BASIS'], MRSCont.vendor, seq);
                 % Save the newly generated .basis file back into the
@@ -307,16 +309,16 @@ switch MRSCont.opts.fit.method
                 error('For LCModel fitting, please explicitly specify a .BASIS or .MAT file in the job file (opts.fit.basisSetFile = ''FILE'').');
             end
             basisSetFile = MRSCont.opts.fit.basisSetFile;
-
-
+            
+            
         end
-
+        
         % Read in the user-supplied control file (if there is one)
         if isfield(MRSCont.opts.fit,'controlFile')
             if ~isempty(MRSCont.opts.fit.controlFile)
                 % Load all control parameters
                 LCMparam = osp_readlcm_control(MRSCont.opts.fit.controlFile);
-
+                
                 % Make some changes to the control file that will apply to
                 % ALL control files
                 LCMparam = osp_editControlParameters(LCMparam, 'lprint', '6');
@@ -360,15 +362,15 @@ switch MRSCont.opts.fit.method
                     
                     % Write control file
                     MRSCont = osp_writelcm_control(MRSCont, kk, 'A', LCMparam);
-
+                    
                 end
-
+                
             else
                 error('The field ''opts.fit.controlFile'' in the job file is specified, but empty.')
             end
-
+            
         else
-
+            
             % If the field does not exist, write default control parameters
             for kk = 1:MRSCont.nDatasets
                 
@@ -390,7 +392,7 @@ switch MRSCont.opts.fit.method
                 LCMparam = osp_editControlParameters(LCMparam, 'namrel', '''Cr+PCr''');
                 LCMparam = osp_editControlParameters(LCMparam, 'ppmst',  ['' sprintf('%4.2f', MRSCont.opts.fit.range(2)) '']);
                 LCMparam = osp_editControlParameters(LCMparam, 'ppmend', ['' sprintf('%4.2f', MRSCont.opts.fit.range(1)) '']);
-
+                
                 % Add water-scaling-related flags only if water reference
                 % data has been provided
                 if MRSCont.flags.hasRef || MRSCont.flags.hasWater
@@ -406,12 +408,14 @@ switch MRSCont.opts.fit.method
                 
             end
         end
-
-
-
+        
 end
 
 end
+
+
+% Additional functions
+
 function [basisSet]=osp_recalculate_basis_specs(basisSet)
     % This function recalculates the basis spectra and ppm-axis of the
     % basis set
