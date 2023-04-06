53556494ab543d8a61a9d7384c9048363db50991
Helge <47348963+HJZollner@users.noreply.github.com>
2023-04-06 09:00:54 -0400

    Comment FitObject methods
    - Added extensive comments to the FitObject methods

--- libraries/FID-A/fitTools/fitModels/Osprey_gLCM/fitClass/@FitObject/excludeBasisFunctionFromFit.m
+++ libraries/FID-A/fitTools/fitModels/Osprey_gLCM/fitClass/@FitObject/excludeBasisFunctionFromFit.m
@@ -1,25 +1,43 @@
 function excludeBasisFunctionFromFit(obj, input)
-    % Sets the 'includeInFit' flag in the 'BasisSets' property to
-    % 0, meaning that the corresponding basis function will be
-    % excluded from in the fit.
-    
-    if ischar(input)
+%%  excludeBasisFunctionFromFit(obj, input)
+%   This method excludes basis functions according to the input
+%
+%   USAGE:
+%       obj.excludeBasisFunctionFromFit(obj, input)
+%
+%   INPUTS:
+%       input   = list of metabolites. %OPTIONS: 'all' or a list of names     
+%       
+%   OUTPUTS:
+%       obj     = OspreyFitObj.
+%
+%   AUTHOR:
+%       Dr. Helge Zoellner (Johns Hopkins University, 2023-03-07)
+%       hzoelln2@jhmi.edu
+%
+%   CREDITS:
+%       This code is based on numerous functions from the FID-A toolbox by
+%       Dr. Jamie Near (McGill University)
+%       https://github.com/CIC-methods/FID-A
+%       Simpson et al., Magn Reson Med 77:23-33 (2017)
+%% Generate index vector
+
+    if ischar(input)                                                                % If is char create a 1x1 cell array
         input = {input};
     end
     
-    if strcmpi(input, 'all')
+    if strcmpi(input, 'all')                                                        % If 'all' include all basis functions of the basis set
         input = obj.BasisSets.names;
     end
     
-    for ii = length(input)
+    for ii = length(input)                                                          % Loop over list of basis function names
         % Check which metabolites are available in the basis set
         % and match the input
-        [metsToInclude, ~, ~] = intersect(obj.BasisSets.names, input, 'stable');
+        [metsToInclude, ~, ~] = intersect(obj.BasisSets.names, input, 'stable');    % Get vector metabolites in the basis set
         for rr = 1:length(metsToInclude)
-            idxToInclude = find(strcmp(metsToInclude{rr}, obj.BasisSets.names));
-            obj.BasisSets.includeInFit(obj.step+1,idxToInclude) = 0;
-        end
-        
-    end
+            idxToExclude = find(strcmp(metsToInclude{rr}, obj.BasisSets.names));    % Get index of basis function to exclude
+            obj.BasisSets.includeInFit(obj.step+1,idxToExclude) = 0;                % Set index to 0 
+        end        
+    end                                                                             % End loop over list of basis function names
 end
         
--- libraries/FID-A/fitTools/fitModels/Osprey_gLCM/fitClass/@FitObject/plotBaselineBasis.m
+++ libraries/FID-A/fitTools/fitModels/Osprey_gLCM/fitClass/@FitObject/plotBaselineBasis.m
@@ -1,16 +1,40 @@
-function plotBaselineBasis(obj, step, plotRange)
-    % default to the provided fit range
-    if nargin < 3
-        plotRange = obj.Options{step}.optimFreqFitRange;
-        if nargin < 2
-            step = obj.step;
-            plotRange = obj.Options{step}.optimFreqFitRange;
-        end
+function plotBaselineBasis(obj, plotRange)
+%%  plotBaselineBasis(obj, plotRange)
+%   This method generates a plot of the baseline basis 
+%
+%   USAGE:
+%       obj.plotBaselineBasis(plotRange)
+%
+%   INPUTS:
+%       plotRange       = set plot range default is optimFreqFitRange
+%
+%   OUTPUTS:
+%       figure
+%
+%   AUTHOR:
+%       Dr. Helge Zoellner (Johns Hopkins University, 2023-03-07)
+%       hzoelln2@jhmi.edu
+%
+%   CREDITS:
+%       This code is based on numerous functions from the FID-A toolbox by
+%       Dr. Jamie Near (McGill University)
+%       https://github.com/CIC-methods/FID-A
+%       Simpson et al., Magn Reson Med 77:23-33 (2017)
+%%  Diverge to default options if required 
+
+    if nargin < 2
+        plotRange = obj.Options{step}.optimFreqFitRange;                    % Set plot range
     end
-        
-    figure;
-    plot(obj.Data.ppm, real(obj.BaselineBasis), 'k');
-    set(gca, 'XDir', 'reverse', 'XLim', plotRange);
+    
+%%  Get fit data from object 
+
+    ppm = obj.Data.ppm;                                                     % Get ppm vector
+    splineBasis = obj.BaselineBasis;                                        % Get baseline basis
+
+%%  Generate figure
+    figure                                                                  % Initialize figure
+    plot(ppm, real(splineBasis), 'k');                                      % Plot baseline basis
+    set(gca, 'XDir', 'reverse', 'XLim', plotRange);                         % Clean appearance
     xlabel('chemical shift (ppm');
     
 end
