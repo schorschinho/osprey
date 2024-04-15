8527cb4523739dca6d62fcbad7a416e1c8175573
schorschinho <goeltzs1@jh.edu>
2024-04-15 11:33:26 -0400

    Update osp_saveLCM.m
    Changes to correctly split a path even if that path was created on a
    different system (with a different 'filesep' placeholder)

--- process/osp_saveLCM.m
+++ process/osp_saveLCM.m
@@ -57,7 +57,7 @@ for kk = 1:MRSCont.nDatasets
     
     % For batch analysis, get the last two sub-folders (e.g. site and
     % subject) to augment the filename, avoiding duplicate output filenames
-    path_split          = regexp(path,filesep,'split');
+    path_split          = splitPath(path);
     if length(path_split) > 2
         name = [path_split{end-1} '_' path_split{end} '_' filename];
     end
@@ -127,7 +127,7 @@ for kk = 1:MRSCont.nDatasets
         
         % For batch analysis, get the last two sub-folders (e.g. site and
         % subject) to augment the filename, avoiding duplicate output filenames
-        path_ref_split          = regexp(path_ref,filesep,'split');
+        path_ref_split          = splitPath(path_ref);
         if length(path_ref_split) > 2
             name_ref = [path_ref_split{end-1} '_' path_ref_split{end} '_' filename_ref];
         end
@@ -154,7 +154,7 @@ for kk = 1:MRSCont.nDatasets
         
         % For batch analysis, get the last two sub-folders (e.g. site and
         % subject) to augment the filename, avoiding duplicate output filenames
-        path_w_split          = regexp(path_w,filesep,'split');
+        path_w_split          = splitPath(path_w);
         if length(path_w_split) > 2
             name_w = [path_w_split{end-1} '_' path_w_split{end} '_' filename_w];
         end
@@ -170,5 +170,34 @@ end
 
 % Set exit flags
 MRSCont.flags.didLCMWrite           = 1;
+
+end
+
+function path_split = splitPath(pathIn)
+% This function correctly splits a path even if that path was created on a
+% different system (e.g., I'm trying to open an MRSCont that was generated
+% on Mac on a Windows machine - in that case, the path will not get split
+% at all because the filesep placeholder on the Windows machine does not
+% catch the (different) file separators in the Mac-generated path
+
+% Is the current filesep placeholder in the path at all?
+isFileSepInPath = contains(pathIn, filesep);
+
+if isFileSepInPath
+    % the filesep placeholder is in the path, proceed as usual
+    path_split          = regexp(pathIn, filesep, 'split');
+else
+    % the filesep placeholder is NOT in the path - choose the respective
+    % other
+    if strcmp(filesep, '/')
+        filesepNew = '\';
+    elseif strcmp(filesep, '\')
+        filesepNew = '/';
+    else
+        error('Unknown filesep');
+    end
+    % Split path with the updated filesep
+    path_split          = regexp(pathIn, filesepNew, 'split');
+end
 
 end
