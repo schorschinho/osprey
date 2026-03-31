6f82465b3316bd18c81a052a831a46ada1a1af49
Helge <47348963+HJZollner@users.noreply.github.com>
2026-03-31 15:02:33 -0400

    Update job files
    Update some descriptions in the jobfile

--- exampledata/mrsi/GE/TE_70/jobMRSI_TE_70_P_NII.m
+++ exampledata/mrsi/GE/TE_70/jobMRSI_TE_70_P_NII.m
@@ -1,4 +1,4 @@
-%% jobSDAT.m
+%% jobMRSI_TE_70_P_NII.m
 %   This function describes an Osprey job defined in a MATLAB script.
 %
 %   A valid Osprey job contains four distinct classes of items:
@@ -49,12 +49,15 @@
 %   individual file names, please specify folders. Metabolite data, water
 %   reference data, and water data need to be located in separate folders.
 %
+%   PREREQUISITES: Download the GE raw MRS data from the OSF
+%   (https://doi.org/10.17605/OSF.IO/VKDPR)
+%
 %   AUTHOR:
-%       Dr. Georg Oeltzschner (Johns Hopkins University, 2019-07-15)
-%       goeltzs1@jhmi.edu
+%       Dr. Helge Zoellner (Johns Hopkins University, 2026-01-15)
+%       hzoelln2@jhmi.edu
 %
 %   HISTORY:
-%       2019-07-15: First version of the code.
+%       2026-01-15: First version of the code.
 
 
 
