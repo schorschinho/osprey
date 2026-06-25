21f8dba1a81c9f2e9301ce1dc8a02cdedfd61cc8
Christopher Davies-Jenkins <c.w.daviesjenkins@gmail.com>
2026-06-25 14:43:26 -0400

    Add error reporting for LCModel analyses

--- fit/code/LCMwrapper/callLCModel.m
+++ fit/code/LCMwrapper/callLCModel.m
@@ -2,6 +2,10 @@ function callLCModel(controlFile, pathLCModelBinary)
 % Wrapper function for LCModel binary
 
 callLCMCommand = ['"' pathLCModelBinary '" < "' controlFile '"'];
-system(callLCMCommand);
+[status, result] = system(callLCMCommand);
+
+if status % If LCModel throws an errpr, capture it and report in the Matlab command window
+    error('LCModel error while executing the following control file:\n%s/n/n%s',controlFile,result);
+end
 
 end
